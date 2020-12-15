4674661f77daba1ebe9c55667c672e35c4fbe2bf
mikebrucato <brucatomike1@gmail.com>
2020-12-15 11:00:55 -0600

    adding parsing handler and listener

--- server.js
+++ server.js
@@ -3,4 +3,14 @@ const path = require('path')
 const fs = require('fs')
 
 var app = express()
-var PORT = 3000
+var port = process.env.port || 3000
+
+// setting up the express app to handle data parsing
+app.use(express.urlncoded({extended: true}))
+app.use(express.json())
+
+
+
+app.listen(PORT, function() {
+    console.log('App listening  on PORT' + PORT)
+})
