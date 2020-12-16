beb0585bb2418242d831d80c4afb9ef06e1f69e2
mikebrucato <brucatomike1@gmail.com>
2020-12-16 14:34:03 -0600

    added readfile function

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const path = require('path')
 const fs = require('fs')
 
 var app = express()
-var port = process.env.port || 3000
+var PORT = process.env.port || 3000
 
 // setting up the express app to handle data parsing
-app.use(express.urlncoded({extended: true}))
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(express.static(__dirname + '/public'))
 
+// fs read file function
+fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err
+
+    let notes = JSON.parse(data)
+})
 
 
 app.listen(PORT, function() {
-    console.log('App listening  on PORT' + PORT)
+    console.log('App listening on PORT ' + PORT)
 })
